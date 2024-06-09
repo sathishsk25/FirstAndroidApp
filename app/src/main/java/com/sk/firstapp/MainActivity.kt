8134e4e00a455396502b40720641b9c5763fd1db
sathish.ks <sathish.ks@zohocorp.com>
2024-06-09 23:02:56 +0530

    Dropdown

--- app/src/main/java/com/sk/firstapp/MainActivity.kt
+++ app/src/main/java/com/sk/firstapp/MainActivity.kt
@@ -4,44 +4,98 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sk.firstapp.ui.theme.FirstAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FirstAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "SK",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               Surface(
+                   modifier = Modifier.fillMaxSize(),
+                   color = MaterialTheme.colorScheme.background
+               ) {
+                   UnitConverter()
+               }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun UnitConverter() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ){
+        Text("Unit Converter", modifier = Modifier.padding(20.dp));
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {} )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+    Row {
+        Box {
+            Button(onClick = { /*TODO */ }) {
+                Text(text = "Input")
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown")
+            }
+            DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenuItem(text = { Text(text = "Option 1")}, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Option 2")}, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Option 3")}, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Option 4")}, onClick = { /*TODO*/ })
+            }
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Box {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Output")
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown")
+            }
+            DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                DropdownMenuItem(text = { Text(text = "Option 1")}, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Option 2")}, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Option 3")}, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Option 4")}, onClick = { /*TODO*/ })
+            }
+        }
+    }
+    Text(text = "Result:")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UnitConverterPreview() {
     FirstAppTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
